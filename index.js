1259b392c0a2926bc111acf477c27ff7ac7dc396
luckyantony <luckyantonyofficial@gmail.com>
2025-03-07 06:55:42 +0300

    Trying arraay map

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function titleCased() {
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +12,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+return tutorials.map((tutorial) => {
+  return tutorial
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  });
 }
